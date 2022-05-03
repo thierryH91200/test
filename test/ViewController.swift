ce102fd6ed3cd7b96d896907e44422304e6920da
thierry <thierry.hentic@gmail.com>
2022-05-03 12:27:31 +0200

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by colombe on 03/05/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
